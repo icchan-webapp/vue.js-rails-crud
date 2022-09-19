9857a4caef0689fd4678c3a3fd429e389a101cf4
icchan-webapp <issei.0157@gmail.com>
2022-09-20 00:32:47 +0900

    feat: employeeの登録・詳細画面の作成 & vue-routerを用いたルーティング設定

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,44 +1,31 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-        <tr>
-          <th>ID</th>
-          <th>name</th>
-          <th>department</th>
-          <th>gender</th>
-        </tr>
-        <tr v-for="e in employees" :key="e.id">
-          <td>{{ e.id }}</td>
-          <td>{{ e.name }}</td>
-          <td>{{ e.department }}</td>
-          <td>{{ e.gender }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import EmployeeIndexPage from 'EmployeeIndexPage.vue'
+import EmployeeDetailPage from 'EmployeeDetailPage.vue'
+import EmployeeNewPage from 'EmployeeNewPage.vue'
 
-export default {
-  data: function () {
-    return {
-      employees: []
-    }
-  },
-  mounted () {
-    axios
-      .get('/api/v1/employees.json')
-      .then(response => (this.employees = response.data))
-  }
-}
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/',
+      component: EmployeeIndexPage },
+    { path: '/employees/:id(\\d+)',
+      name: 'EmployeeDetailPage',
+      component: EmployeeDetailPage },
+    { path: '/employees/new',
+      name: 'EmployeeNewPage',
+      component: EmployeeNewPage }
+  ]
+})
+
+export default router
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -3,5 +3,6 @@ import App from "../app.vue";
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = createApp(App);
+  app.use(App);
   app.mount("#vue-app");
 });
